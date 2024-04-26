5c248722d0ec54f2f190525b3ec9d2d2ee39bd81
uni marx <uniwuni@protonmail.com>
2024-04-26 14:36:20 +0200

    initial

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to uniwuni/rdf-archiver
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/uniwuni/rdf_archiver_test.clj
@@ -0,0 +1,7 @@
+(ns uniwuni.rdf-archiver-test
+  (:require [clojure.test :refer :all]
+            [uniwuni.rdf-archiver :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
